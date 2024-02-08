e292b111976d6883c45d4c785e184c36d3158186
brugodoy <139875364+brugodoy@users.noreply.github.com>
2024-02-08 11:01:39 -0300

    Update bdbd.sql

--- bdbd.sql
+++ bdbd.sql
@@ -25,10 +25,11 @@ CREATE TABLE imoveis (
 );
 
 -- amostra
-INSERT INTO imoveis (sigla, titulo, logradouro, numero, bairro, complemento, zona) VALUES
+INSERT INTO 'imoveis' ('sigla', 'titulo', 'logradouro', 'numero', 'bairro', 'complemento', 'zona') VALUES
 ('NE123456789123', 'rua', 'bicudo leme', '20', 'boa vista', 'sala 1', 'zc1'),
 ('NE223456789123', 'rua', 'bicudo leme', '40', 'boa vista', 'sala 3', 'zop'),
 ('NE323456789123', 'rua', 'bicudo leme', '60', 'boa vista', 'sala 9', 'zom');
+COMMIT;
 
 -- Criação da tabela de saida
 CREATE TABLE saida (
